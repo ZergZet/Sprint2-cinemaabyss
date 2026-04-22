76195ef107c83ebd0009b8e7e7eeee75a56073b9
ZergZet <sergio911@yandex.ru>
2026-04-22 14:01:29 +0300

    Update wait-for-kafka.sh

--- tests/postman/wait-for-kafka.sh
+++ tests/postman/wait-for-kafka.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 echo "Waiting for Kafka to be ready..."
-# Проверяем, что Kafka отвечает на --list
 until docker exec cinemaabyss-kafka kafka-topics --bootstrap-server kafka:9092 --list > /dev/null 2>&1; do
   echo "Kafka not ready, sleeping 2s..."
   sleep 2
 done
 
-echo "Kafka is ready. Ensuring topics exist..."
+echo "Kafka is ready. Creating topics if not exist..."
 for topic in movie-events user-events payment-events; do
   docker exec cinemaabyss-kafka kafka-topics --bootstrap-server kafka:9092 --create --if-not-exists --topic "$topic" --partitions 1 --replication-factor 1
 done
